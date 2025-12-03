991ae467e50434dbf920b612c85e38de51dddef3
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-03 11:04:32 -0500

    For Loop: Sum Calculator

---
+++ Practice/src/SumCalculator.java
@@ -0,0 +1,10 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        int maxNumber = 4;
+        int sum = 0;
+        for (int i = 0; i < maxNumber; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
