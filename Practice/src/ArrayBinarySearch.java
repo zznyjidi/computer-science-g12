a297d8bab8f84be30ae650d98064874b61f030fe
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-05 15:25:30 -0500

    Array: Binary Search with Boundary

--- Practice/src/ArrayBinarySearch.java
+++ Practice/src/ArrayBinarySearch.java
@@ -9,36 +9,18 @@ public class ArrayBinarySearch {
     }
 
     public static int binarySearch(int[] array, int target) {
-
-        // Gate Keeping
-        if (target < array[0] || target > array[array.length-1]) {
-            return -1;
-        }
-
-        int halfIndex = array.length / 2;
-        int offset = halfIndex / 2;
-        while (true) {
-            // Match
-            if (array[halfIndex] == target) {
-                return halfIndex;
-            // Middle No Match
-            } else if (
-                (halfIndex - 1 >= 0 && halfIndex + 1 < array.length) 
-                && (array[halfIndex - 1] < target && array[halfIndex + 1] > target)
-            ) {
-                return -1;
-            // Search Left
-            } else if (array[halfIndex] > target) {
-                halfIndex -= offset;
-                offset /= 2;
-            // Search Right
+        int left = 0;
+        int right = array.length - 1;
+        while (right-left > 1) {
+            int middle = (left + right) / 2;
+            if (array[middle] == target) {
+                return middle;
+            } else if (array[middle] > target) {
+                right = middle;
             } else {
-                halfIndex += offset;
-                offset /= 2;
-            }
-            if (offset == 0) {
-                offset = 1;
+                left = middle;
             }
         }
+        return -1;
     }
 }
