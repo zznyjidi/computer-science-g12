91dded1a0dc9d737b5777ad76a45659b0836ef38
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-10 12:10:36 -0500

    Factor: prime and factor check

--- Practice/src/FactorFinder.java
+++ Practice/src/FactorFinder.java
@@ -5,9 +5,23 @@ import java.util.Set;
 public class FactorFinder {
     public static void main(String[] args) {
         int number = SumCalculator.getPositiveNumber("> ");
-        Object[] factors = calculateFactor(number).toArray();
-        Arrays.sort(factors);
-        IO.println(Arrays.toString(factors));
+        Set<Integer> factors = calculateFactor(number);
+        boolean numberIsPrime = isPrime(factors);
+
+        IO.println(number + (numberIsPrime? " is a prime. " : " is not a prime. "));
+        IO.println("Factors: " + toSortedString(factors));
+
+        if (!numberIsPrime) {
+            IO.println("Input a positive number to find if it is a factor \nInput a negative number or 0 to exit");
+            while (true) {
+                int inputNumber = SumCalculator.getNumber("> ");
+                if (inputNumber > 0) {
+                    IO.println(inputNumber + " is" + (isFactor(factors, inputNumber)?" ":" not ") + "a factor of " + number + ". ");
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     public static Set<Integer> calculateFactor(int number) {
@@ -26,5 +40,27 @@ public class FactorFinder {
         }
 
         return factors;
+    }
+
+    public static boolean isPrime(int number) {
+        return isPrime(calculateFactor(number));
+    }
+
+    public static boolean isPrime(Set<Integer> factors) {
+        return factors.size() == 2;
+    }
+
+    public static boolean isFactor(int bigNumber, int number) {
+        return isFactor(calculateFactor(bigNumber), number);
+    }
+
+    public static boolean isFactor(Set<Integer> factors, int number) {
+        return factors.contains(number);
+    }
+
+    public static <T> String toSortedString(Set<T> set) {
+        Object[] array = set.toArray();
+        Arrays.sort(array);
+        return Arrays.toString(array);
     }
 }
