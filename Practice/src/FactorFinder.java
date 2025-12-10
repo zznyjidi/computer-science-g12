284b0a5902b915748bec0ec5a1b83dd24faf24a6
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-10 11:15:30 -0500

    Set: Factor Calculator - Fix 1

--- Practice/src/FactorFinder.java
+++ Practice/src/FactorFinder.java
@@ -11,7 +11,10 @@ public class FactorFinder {
     public static Set<Integer> calculateFactor(int number) {
         Set<Integer> factors = new HashSet<>();
 
-        for (int i = 1; i < number / 2; i++) {
+        factors.add(1);
+        factors.add(number);
+
+        for (int i = 2; i < number / 2; i++) {
             if (number % i == 0) {
                 factors.add(i);
                 factors.add(number/i);
