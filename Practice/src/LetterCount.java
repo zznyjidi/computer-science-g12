bd3a8ed32989e43bda6f6a9c2d030cc7e47b7f20
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-15 11:33:35 -0500

    Count Letters: Total Frequency

--- Practice/src/LetterCount.java
+++ Practice/src/LetterCount.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LetterCount {
     public static void main(String[] args) {
+        List<Map<Character, Integer>> allFrequencies = new ArrayList<>();
         while (true) {
             String input = IO.readln("Word? > ");
             if (input.equals("")) break;
 
             Map<Character, Integer> letterFrequency = countLetter(input);
+            allFrequencies.add(letterFrequency);
 
             IO.println(countLetter(input));
             IO.println("The most used letters in the word " + input + " is " + findHighestValue(letterFrequency));
         }
+        Map<Character, Integer> totalFrequency = mergeFrequencyMap(allFrequencies);
+        IO.println(totalFrequency);
+        IO.println("The most used letters in all words is " + findHighestValue(totalFrequency));
     }
 
     public static Map<Character, Integer> countLetter(String word) {
@@ -23,6 +30,21 @@ public class LetterCount {
             letters.put(Character.toLowerCase(c), letters.getOrDefault(c, 0) + 1);
 
         return letters;
+    }
+
+    public static <K> Map<K, Integer> mergeFrequencyMap(List<Map<K, Integer>> maps) {
+        Map<K, Integer> totalFrequency = new HashMap<>();
+
+        for (Map<K,Integer> map : maps) {
+            for (Map.Entry<K, Integer> entry : map.entrySet()) {
+                totalFrequency.put(
+                    entry.getKey(), 
+                    totalFrequency.getOrDefault(entry.getKey(), 0) + entry.getValue()
+                );
+            }
+        }
+
+        return totalFrequency;
     }
 
     public static <K> K findHighestValue(Map<K, Integer> inputMap) {
