c5083eb6d0e0a788f9ecc9742bf100acf0d4abed
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-16 13:16:44 -0500

    Password Manager: AES Encryption

--- Practice/src/PasswordManager.java
+++ Practice/src/PasswordManager.java
@@ -3,15 +3,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
 public class PasswordManager {
 
     static File passwordFile = new File("passwords.txt");
+    static SecretKey key;
+    static String secretSalt = "JCg5Ug7QgXdd2ZieDXng";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        key = AESEncryption.getKeyWithPassword(IO.readln("Master Password: "), secretSalt);
+
         Map<String, Map<String, String>> passwords = new HashMap<>();
         loadPasswords(passwords, passwordFile);
 
@@ -23,7 +36,15 @@ public class PasswordManager {
                 case "exit" -> running = false;
                 case "add" -> {
                     try {
-                        addPassword(passwords, input[1], input[2], input[3]);
+                        addPassword(passwords, input[1], input[2], encryptPassword(input[3]));
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        IO.println("Not Enough Input. ");
+                        e.printStackTrace();
+                    }
+                }
+                case "get" -> {
+                    try {
+                        IO.println(decryptPassword(passwords.get(input[1]).get(input[2])));
                     } catch (ArrayIndexOutOfBoundsException e) {
                         IO.println("Not Enough Input. ");
                         e.printStackTrace();
@@ -33,7 +54,7 @@ public class PasswordManager {
                     for (Map.Entry<String, Map<String, String>> websiteEntry : passwords.entrySet()) {
                         IO.println(websiteEntry.getKey() + " {");
                         for (Map.Entry<String, String> passwordEntry : websiteEntry.getValue().entrySet()) {
-                            IO.println(String.format("    %-30s %s", passwordEntry.getKey(), passwordEntry.getValue()));
+                            IO.println(String.format("    %s", passwordEntry.getKey()));
                         }
                         IO.println("}");
                     }
@@ -50,7 +71,7 @@ public class PasswordManager {
                 addPassword(passwords, file.next(), file.next(), file.next());
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            IO.println("Password Save not found. Starting Fresh. ");
         }
     }
 
@@ -80,5 +101,31 @@ public class PasswordManager {
         if (!passwords.containsKey(website))
             passwords.put(website, new HashMap<>());
         passwords.get(website).put(username, password);
+    }
+
+    public static String encryptPassword(String password) {
+        try {
+            byte[] iv = AESEncryption.generateIV();
+            return AESEncryption.base64Encoder.encodeToString(iv)
+                    + "-" + AESEncryption.encryptString(password, key, iv);
+        } catch (InvalidKeyException | NoSuchAlgorithmException
+                | NoSuchPaddingException | InvalidAlgorithmParameterException
+                | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            return "ENCRYPT_FAIL";
+        }
+    }
+
+    public static String decryptPassword(String encrypted) {
+        try {
+            String[] values = encrypted.split("-");
+            return AESEncryption.decryptString(values[1], key, AESEncryption.base64Decoder.decode(values[0]));
+        } catch (InvalidKeyException | NoSuchAlgorithmException
+                | NoSuchPaddingException | InvalidAlgorithmParameterException
+                | IllegalBlockSizeException | BadPaddingException
+                | ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return "DECRYPT_FAIL";
+        }
     }
 }
