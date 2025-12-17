ce00b00e521788eeee37fd34877e9e9303b84afd
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-12-17 12:33:25 -0500

    Factorial: Formula

--- Practice/src/Factorial.java
+++ Practice/src/Factorial.java
@@ -1,8 +1,9 @@
 public class Factorial {
     public static void main(String[] args) {
         int number = SumCalculator.getPositiveNumber("> ");
-        IO.println(factorial(number));
-        IO.println(factorialRecursion(number));
+        IO.println("Loop: " + factorial(number));
+        IO.println("Recursion: " + factorialRecursion(number));
+        IO.println("Formula: " + factorialFormula(number));
     }
 
     public static int factorial(int number) {
@@ -17,5 +18,9 @@ public class Factorial {
         if (number == 1)
             return 1;
         return factorialRecursion(number - 1) + number;
+    }
+
+    public static int factorialFormula(int number) {
+        return (1 + number) * number / 2;
     }
 }
