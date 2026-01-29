935ff50ccb8bdaf2fe563a509f56ce44f80c1f8c
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-01-29 13:06:16 -0500

    fix: offset larger than 26

--- Practice/src/CaesarCipher.java
+++ Practice/src/CaesarCipher.java
@@ -1,5 +1,6 @@
 public class CaesarCipher {
     public static String encode(String original, int offset) {
+        offset = offset % 26;
         StringBuilder encoded = new StringBuilder();
         for (int i = 0; i < original.length(); i++) {
             encoded.append(shiftIndex(original.charAt(i), offset));
@@ -11,7 +12,7 @@ public class CaesarCipher {
         return encode(encoded, -offset);
     }
 
-    public static char shiftIndex(char c, int offset) {
+    private static char shiftIndex(char c, int offset) {
         if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')))
             return c;
         int newIndex = c + offset;
@@ -23,9 +24,13 @@ public class CaesarCipher {
     }
 
     public static void main(String[] args) {
-        String value = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
-        String encoded = encode(value, 7);
-        IO.println(encoded);
-        IO.println(decode(encoded, 7));
+        while (true) {
+            String value = IO.readln("> ");
+            int offset = Integer.parseInt(IO.readln("Offset: "));
+
+            String encoded = encode(value, offset);
+            IO.println(encoded);
+            IO.println(decode(encoded, offset));
+        }
     }
 }
